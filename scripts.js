2091b43d0dd7dbf258fb84ca5682fc0b5bfabed1
Pedro Henrique Boccardo de Abreu <pedrohenriqueboccardodeabreu@gmail.com>
2025-05-05 20:05:03 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,66 @@
 // Variável "ID" inicial dos itens da tarefa
 var id = 0
 
+// Função "adicionar tarefa"
+function adicionar() {
+    // Somador de IDs
+    id += 1
+
+    // Selecionando o input da pessoa
+    const input = document.querySelector("#input");
+    const lfazer = document.querySelector("#lFazer");
+
+    // Criando os DIVs das propriedades da linha da tarefa
+    tarefa = document.createElement("div");
+    var quadrado = document.createElement("div");
+    var texto = document.createElement("div");
+    var lapis = document.createElement("div");
+    var lixeira = document.createElement("div");
+    
+    // 
+    if (lfazer.classList.contains("oculto")) {
+        lfazer.classList.remove("oculto");
+    }
+
+    // Propriedade "tarefa"
+    tarefa.setAttribute("class", "tarefa");
+    tarefa.setAttribute("id", "t" + id);
+    lfazer.appendChild(tarefa);
+
+    // Propriedade "quadrado"
+    quadrado.setAttribute("class", "fa fa-square");
+    quadrado.setAttribute("id", "quadrado" + id);
+    quadrado.setAttribute("onclick", "checkar(this)");
+    tarefa.appendChild(quadrado);
+
+    // Propriedade "texto"
+    texto.setAttribute("class", "textoLista");
+    texto.setAttribute("id", "texto" + id);
+    texto.innerText = input.value;
+    tarefa.appendChild(texto);
+
+    // Propriedade "lapis"
+    lapis.setAttribute("class", "fa fa-pencil-square");
+    lapis.setAttribute("id", "lapis" + id);
+    lapis.setAttribute("onclick", "editar(this)");
+    tarefa.appendChild(lapis);
+
+    // Propriedade "lixeira"
+    lixeira.setAttribute("class", "fa fa-trash-can");
+    lixeira.setAttribute("id", "apagar" + id);
+    lixeira.setAttribute("onclick", "excluir(this)");
+    tarefa.appendChild(lixeira);
+}
+
+document.getElementById("input").addEventListener("keydown", function(evento) {
+    if (evento.key === "Enter") {
+        adicionar();
+    }
+});
+
 // Botão "editar" da tarefa
 function editar(textoAlterar) {
     // Chamando e trocando os valores do texto na aba de edição
-    
     const textoId = textoAlterar.id.replace("lapis", "texto");
     const textoTarefa = document.getElementById(textoId);
     var textoAtual = document.getElementById(textoId).innerHTML;
@@ -23,7 +79,13 @@ function editar(textoAlterar) {
             var inputValue = document.querySelector("#editarInput").value;
             textoTarefa.innerText = inputValue;
         }
-      });
+        document.getElementById("input").addEventListener("keydown", function(evento) {
+            if (evento.key === "Enter") {
+                var inputValue = document.querySelector("#editarInput").value;
+                textoTarefa.innerText = inputValue;
+            }
+        });
+    });
 }
 
 // Botão "excluir" da tarefa
@@ -44,107 +106,49 @@ function excluir(button) {
             const tarefa = document.querySelector("#" + tarefaid)
             tarefa.remove();
         }
-      });  
-}
+    });  
 
- //
- const lfazer = document.querySelector("#lFazer");
- if(lfazer.innerHTML=="") {
-   lfazer.classList.add("oculto");
- }
-
- //
- const lfeito = document.querySelector("#lFeito");
- if(lfeito.innerHTML=="") {
-   lfeito.classList.add("oculto");
- }    
-
-// Função "adicionar tarefa"
-function adicionar() {
-    // Somador de IDs
-    id += 1
-
-    // Selecionando o input da pessoa
-    const input = document.querySelector("#input");
+     //
     const lfazer = document.querySelector("#lFazer");
-
-    // Criando os DIVs das propriedades da linha da tarefa
-    tarefa = document.createElement("div");
-    var quadrado = document.createElement("div");
-    var texto = document.createElement("div");
-    var lapis = document.createElement("div");
-    var lixeira = document.createElement("div");
-    
-    // 
-    if(lfazer.getElementsByClassName("oculto")){
-        lfazer.classList.remove("oculto");
+    if(lfazer.innerHTML==="") {
+        lfazer.classList.add("oculto");
     }
 
-    // Propriedade "tarefa"
-    tarefa.setAttribute("class", "tarefa");
-    tarefa.setAttribute("id", "t" + id);
-    lfazer.appendChild(tarefa);
-
-    // Propriedade "quadrado"
-    quadrado.setAttribute("class", "fa fa-square");
-    quadrado.setAttribute("id", "quadrado" + id);
-    quadrado.setAttribute("onclick", "checkar(this)")
-    tarefa.appendChild(quadrado);
-
-    // Propriedade "texto"
-    texto.setAttribute("class", "textoLista");
-    texto.setAttribute("id", "texto" + id);
-    texto.innerText = input.value;
-    tarefa.appendChild(texto);
-
-    // Propriedade "lapis"
-    lapis.setAttribute("class", "fa fa-pencil-square");
-    lapis.setAttribute("id", "lapis" + id);
-    lapis.setAttribute("onclick", "editar(this)")
-    tarefa.appendChild(lapis);
-
-    // Propriedade "lixeira"
-    lixeira.setAttribute("class", "fa fa-trash-can");
-    lixeira.setAttribute("id", "apagar" + id);
-    lixeira.setAttribute("onclick", "excluir(this)");
-    tarefa.appendChild(lixeira);
+    //
+    const lfeito = document.querySelector("#lFeita");
+    if(lfeito.innerHTML==="") {
+        lfeito.classList.add("oculto");
+    }    
 }
 
 // Função "checkar tarefa"
 function checkar(checkarTarefa) {
-    // Chamando a DIV "tudo" do HTML
-    const tudo = document.querySelector("#tudo");
-    const lFeita = document.querySelector("lFeita");     
-
-    // Elementos das tarefas feitas
-    var quadrado = document.getElementById(checkarTarefa.id);
-    var textoId = checkarTarefa.id.replace("quadrado", "texto");
-    var texto = document.getElementById(textoId)
-    quadrado.setAttribute("class", "fa fa-check-square");
-    texto.setAttribute("class", "textoLista riscado")
-
-    // Chamando o ID da tarefa
-    var tarefaId = checkarTarefa.id.replace("quadrado", "t");
-    var tarefa = document.getElementById(tarefaId);
-
-    // Clonando a tarefa
-    var tarefaCheckada = tarefa.cloneNode(true);
-
-    // Criando a DIV onde a tarefa concluída ficará
-    const lfeita = document.createElement("div");
-    lfeita.appendChild(tarefaCheckada);
-
-    // Atribuindo a área das tarefas feitas
-    lfeita.setAttribute("class", "listaTarefas");
-    lfeita.setAttribute("id", "lFeita");
-    tudo.appendChild(lfeita);
-
-    lfeitoa.appendChild(tarefaCheckada);
-
-    if(lFeita.getElementsByClassName("oculto")){
-        lFeita.classList.remove("oculto");
+    const quadrado = document.getElementById(checkarTarefa.id);
+    const textoId = checkarTarefa.id.replace("quadrado", "texto");
+    const texto = document.getElementById(textoId);
+    const tarefaId = checkarTarefa.id.replace("quadrado", "t");
+    const tarefa = document.getElementById(tarefaId);
+    
+    const lfazer = document.querySelector("#lFazer");
+    const lfeita = document.querySelector("#lFeita");
+    
+    if (quadrado.classList.contains("fa-square")) {
+        // Marcar como feita
+        quadrado.classList.remove("fa-square");
+        quadrado.classList.add("fa-check-square");
+        texto.classList.add("riscado");
+        lfeita.appendChild(tarefa);
+    
+        lfeita.classList.remove("oculto");
+        if (lfazer.children.length === 0) lfazer.classList.add("oculto");
+    } else {
+        // Marcar como não feita
+        quadrado.classList.remove("fa-check-square");
+        quadrado.classList.add("fa-square");
+        texto.classList.remove("riscado");
+        lfazer.appendChild(tarefa);
+    
+        lfazer.classList.remove("oculto");
+        if (lfeita.children.length === 0) lfeita.classList.add("oculto");
     }
-
-    // Remover a tarefa original depois de "checkada"
-    tarefa.remove()
 }
